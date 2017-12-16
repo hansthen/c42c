40a74fa750c251d3b24eb75edee02c2a6fd2c677
hansthen <hans.then@gmail.com>
2017-12-16 10:21:36 +0100

    Fix bugs found after testing
    1. Flask uses keyword arguments, so parameter names must match with
       cache wrapper function.
    2. I misread the input data formats.
    3. Added the token.
    4. Setup logging to debug level.
    5. Return a json string instead of a dict.

--- main.py
+++ main.py
@@ -4,27 +4,29 @@ import logging
 from datetime import datetime, timedelta
 from threading import Lock, Thread
 from time import sleep
+import json
 
+logging.basicConfig(level='DEBUG')
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-TOKEN='FIXME'
+TOKEN='d6126cc0239dcdc1b300bf34b8dcf2ea3a303901'
 EXPIRY=4.2
 
 cache = {}
 lock = Lock()
 
 def cacheme(fn):
-    def wrapper(key):
+    def function(event_id):
         with lock:
             try:
-                _, value = cache[key]
+                _, value = cache[event_id]
                 return value
             except KeyError:
-                expiry, value = (datetime.now() + timedelta(minutes=EXPIRY), fn(key))
-                cache[key] = (expiry, value)
+                expiry, value = (datetime.now() + timedelta(minutes=EXPIRY), fn(event_id))
+                cache[event_id] = (expiry, value)
                 return value
-    return wrapper
+    return function
 
 def cleanup():
     for key in cache:
@@ -34,25 +36,26 @@ def cleanup():
     sleep(1)
 
 
-@app.route('/events-with-subscriptions/<id>')
+@app.route('/events-with-subscriptions/<event_id>')
 @cacheme
 def events_with_subscriptions(event_id):
     headers = { 'Accept': 'application/json',
                 'Content-type': 'application/json',
                 'Authorization': 'Token {}'.format(TOKEN) }
     url_evt = 'https://demo.calendar42.com/api/v2/events/{}'.format(event_id)
-    url_subs = 'https://demo.calendar42.com/api/v2/events-subscriptions/'
+    url_subs = 'https://demo.calendar42.com/api/v2/event-subscriptions/'
     response = requests.get(url_evt, headers=headers)
+    response.raise_for_status()
     details = response.json()
-    payload = { 'event_ids': event_id }
-    response = request(url_subs, headers=headers, params=payload)
+    payload = { 'event_ids': '[{}]'.format(event_id), 'limit': '10' }
+    response = requests.get(url_subs, headers=headers, params=payload)
     subs = response.json()
 
     result = { 'id': event_id,
-               'title': details['titles'],
-               'names': [sub['name'] for sub in subs] }
+               'title': details['data'][0]['title'],
+               'names': ["{last_name}, {first_name}".format(**sub['actor']) for sub in subs['data']] }
 
-    return result
+    return json.dumps(result)
 
 
 if __name__ == '__main__':
